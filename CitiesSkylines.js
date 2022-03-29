7348bfd984d46749a14bf131b014553303898934
Leandro Hyun <leandro.souza@icen.ufpa.br>
2022-03-28 23:16:58 -0300

    first commit

---
+++ CitiesSkylines.js
@@ -0,0 +1,5 @@
+/*Na descrição da tarefa diz apenas um programa inicial, 
+então eu não sabia o que fazer exatamente
+e não tenho certeza se é isso mesmo. T.T */
+
+document.write("Planejamento Do Jogo Cities Skylines")
